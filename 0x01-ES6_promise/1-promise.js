79a3419979b2b360259dfffdf1866204278dd85e
therealmahmoud <moha91875460@gmail.com>
2024-06-26 22:01:25 +0300

    Updated

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,6 +4,6 @@ export default function getResponseFromAPI(success) {
     resolve({ status: 200, body: 'Success' });
   }
   else {
-    reject(Error("The fake API is not working currently"));
+    reject(new Error("The fake API is not working currently"));
   }
   })};
