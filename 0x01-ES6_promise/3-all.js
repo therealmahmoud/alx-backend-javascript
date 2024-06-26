51373822116a55f24957df73e4c1b3161f5b6576
therealmahmoud <moha91875460@gmail.com>
2024-06-27 00:59:57 +0300

    Updated

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,8 @@
 import {uploadPhoto, createUser} from "./utils";
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log(photo.body, user.firstName, user.lastName);
     })
