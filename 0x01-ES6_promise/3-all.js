5e50239bca8d5c3e2bff05a13796a0d747fd835e
therealmahmoud <moha91875460@gmail.com>
2024-06-27 01:53:12 +0300

    Updated

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-   .then(([photo, user]) => {
-      console.log(photo.body, user.firstName, user.lastName);
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
-   .catch(() => console.log("Signup system offline"));
+    .catch(() => console.log("Signup system offline"));
 }
