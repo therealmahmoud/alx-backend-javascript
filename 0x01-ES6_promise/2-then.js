4b9ea10399729b1a01859fc5502b349fb329c638
therealmahmoud <moha91875460@gmail.com>
2024-06-26 23:12:36 +0300

    Updated

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -8,7 +8,7 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
     }
     else {
-      reject(new Error)
+      reject(new Error())
     }
   })
 }
