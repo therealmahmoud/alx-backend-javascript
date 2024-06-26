8cb48643b60d8a93ab679c7f9aeccb23d644ce67
therealmahmoud <moha91875460@gmail.com>
2024-06-27 01:04:04 +0300

    Updated

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,5 +6,5 @@ export default function handleProfileSignup() {
     .then((result) => {
       console.log(`${result[0].body}, ${result[1].firstName}, ${result[1].lastName}`);
     })
-    .catch(() => console.error("Signup system offline"));
+    .catch(() => console.log("Signup system offline"));
 }
