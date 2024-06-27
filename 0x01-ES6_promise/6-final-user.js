cee8f95739e778b06c98350bbdb7475334d9aded
therealmahmoud <moha91875460@gmail.com>
2024-06-27 03:52:26 +0300

    Updated

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,14 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-  .all([signUpUser(), uploadPhoto()])
-  .then((result) => {
-    console.log(`${result[0].firstName} ${result[1].lastName} ${result[2].fileName}`);
-  })
-  .catch(() => new Error());
-};
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
+}
