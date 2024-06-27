4e157c073f798efe17ab8b4bc00f2a3edaf562a6
therealmahmoud <moha91875460@gmail.com>
2024-06-27 03:03:40 +0300

    Updated

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,3 @@
 export default function uploadPhoto(filename) {
-  return Promise.reject(Error("guillaume.jpg cannot be processed"));
+  return Promise.reject(new Error("guillaume.jpg cannot be processed"));
 }
