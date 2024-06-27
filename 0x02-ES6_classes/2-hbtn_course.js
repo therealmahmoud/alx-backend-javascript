0c468fe5507fde5c85decc8812614bd20cb480c6
therealmahmoud <moha91875460@gmail.com>
2024-06-28 02:59:26 +0300

    Updated

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,16 +8,18 @@ export default class HolbertonCourse {
     return this._name
   }
   set name(val) {
-    if (typeof val !== 'string')
+    if (typeof val !== 'string'){
       throw new TypeError('Name must be a string');
+    }
     this._name = val
   }
   get length() {
     return this._length
   }
   set length(val) {
-    if (typeof val !== Number)
+    if (typeof val !== Number){
       throw new TypeError('Length must be a number');
+    }
     this._length = val
   }
   get students(){
@@ -25,6 +27,9 @@ export default class HolbertonCourse {
   }
   set students(val) {
     if (!(val instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!val.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = val;
