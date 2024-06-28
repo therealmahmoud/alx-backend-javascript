3d413ecb5c873b4d53eb9c544de493a06e62a325
therealmahmoud <moha91875460@gmail.com>
2024-06-28 03:13:46 +0300

    Updated

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,43 +1,43 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(val) {
-    if (typeof val !== 'string'){
+  set name(value) {
+    if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = val;
+    this._name = value;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(val) {
-    if (typeof val !== Number){
+  set length(value) {
+    if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = val;
+    this._length = value;
   }
 
   get students() {
     return this._students;
   }
 
-  set students(val) {
-    if (!(val instanceof Array)) {
+  set students(value) {
+    if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
     }
-    if (!val.every((student) => typeof student === 'string')) {
+    if (!value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    this._students = val;
+    this._students = value;
   }
 }
--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,6 +1,20 @@
 export default class Currency {
   constructor(code, name) {
     this._code = code;
-    
+    this._name = name;
+  }
+  displayFullCurrency(name, code) {
+    return `${this._name} (${this._code})`
+  }
+  get code() {
+    return this._code
+  }
+
+  set code() {
+
+  }
+
+  get name() {
+    return this._name
   }
 }
