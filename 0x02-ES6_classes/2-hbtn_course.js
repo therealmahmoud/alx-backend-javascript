32174185b4173c957c8c29c448dbf5e8cc1a4cff
therealmahmoud <moha91875460@gmail.com>
2024-06-28 03:05:29 +0300

    Updated

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name
-    this._length = length
-    this._students = students
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
   get name() {
     return this._name
