01d1657c7d747c1ff495ace7160d8535c13313f0
therealmahmoud <moha91875460@gmail.com>
2024-06-28 03:26:34 +0300

    Updated

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -4,24 +4,24 @@ export default class Currency {
     this.name = name;
   }
 
-  displayFullCurrency() {
-    return `${this.name} (${this.code})`;
-  }
-
+  
   get code() {
     return this._code;
   }
-
+  
   set code(val) {
     this._code = val;
   }
-
+  
   get name() {
     return this._name;
   }
-
+  
   set name(val) {
     this._name = val;
   }
-
+  
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
