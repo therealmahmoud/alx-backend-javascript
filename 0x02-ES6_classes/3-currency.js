90b80ddc165bc5a0eaaf5404f67d56cecd5d4e17
therealmahmoud <moha91875460@gmail.com>
2024-06-28 03:18:48 +0300

    Updated

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,18 +3,31 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   displayFullCurrency(name, code) {
     return `${this._name} (${this._code})`
   }
+
   get code() {
     return this._code
   }
 
-  set code() {
-
+  set code(val) {
+    if (val !== 'string'){
+      throw new TypeError('Code must be string')
+    }
+    this._code = val
   }
 
   get name() {
     return this._name
   }
+
+  set name(val) {
+    if (val !== 'string'){
+      throw new TypeError('Name must be string')
+    }
+    this._name = val
+  }
+
 }
