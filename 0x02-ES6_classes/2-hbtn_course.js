faf0e601161fa1fe436b995f42216a2d34c0fb4c
therealmahmoud <moha91875460@gmail.com>
2024-06-28 03:09:10 +0300

    Updated

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,27 +4,33 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
   get name() {
     return this._name;
   }
+
   set name(val) {
     if (typeof val !== 'string'){
       throw new TypeError('Name must be a string');
     }
     this._name = val;
   }
+
   get length() {
     return this._length;
   }
+
   set length(val) {
     if (typeof val !== Number){
       throw new TypeError('Length must be a number');
     }
     this._length = val;
   }
+
   get students() {
     return this._students;
   }
+
   set students(val) {
     if (!(val instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
