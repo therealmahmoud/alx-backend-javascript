eac4ed0295a96f3a1c8e777e92102fec2efa1982
therealmahmoud <moha91875460@gmail.com>
2024-07-02 20:46:41 +0300

    Updated

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,12 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
+* @returns
+*/
 export default function getListStudentIds(arr) {
   if (arr instanceof Array) {
     return arr.map((arr) => arr.id);
