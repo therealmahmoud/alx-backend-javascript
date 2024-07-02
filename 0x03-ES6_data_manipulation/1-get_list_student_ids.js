cd1c9258c43b67ac6c1ae2ff8eb0449b3ed76568
therealmahmoud <moha91875460@gmail.com>
2024-07-02 20:05:43 +0300

    Updated

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+export default function getListStudentIds(arr = []) {
+  const obj = new Map();
+  if (arr !== Array) {
+    return [];
+  }
+  else {
+    arr.obj(items) => {
+      return items;
+    }
+  }
+}
