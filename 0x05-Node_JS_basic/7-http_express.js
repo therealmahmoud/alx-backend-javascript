a0138543ab63f95b435785e6d0624c15f26340e0
therealmahmoud <moha91875460@gmail.com>
2024-08-21 01:50:22 +0300

    Updated

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,31 @@
+// 7-http_express.js
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const databaseFilePath = process.argv[2];
+
+  res.write('This is the list of our students\n');
+
+  if (databaseFilePath) {
+    countStudents(databaseFilePath)
+      .then(() => res.end())
+      .catch((error) => {
+        res.write(`${error.message}\n`);
+        res.end();
+      });
+  } else {
+    res.write('Cannot load the database\n');
+    res.end();
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
