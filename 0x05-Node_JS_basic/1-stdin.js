bf3bccedd813599a73d514cba367451a642b17e1
therealmahmoud <moha91875460@gmail.com>
2024-08-20 11:23:54 +0300

    Updated

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const line = process.stdin.read();
+  if (line) {
+    process.stdout.write(`your name is: ${line}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
