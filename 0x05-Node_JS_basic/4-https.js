3e6961cb96a9ef88e4f62148e47d4ecf7fbd5ba9
therealmahmoud <moha91875460@gmail.com>
2024-08-20 21:31:00 +0300

    Updated

--- 0x05-Node_JS_basic/4-https.js
+++ 0x05-Node_JS_basic/4-https.js
@@ -10,7 +10,7 @@ const app = http.createServer((req, res) => {
 });
 
 app.listen(port, host, () => {
-  console.log(`Server running at https://${host}/${port}`);
+  console.log('...');
 });
 
 module.exports = app;
