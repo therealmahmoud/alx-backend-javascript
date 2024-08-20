091f849601966dfad45f3258e14cc3572af40f8c
therealmahmoud <moha91875460@gmail.com>
2024-08-20 11:30:07 +0300

    Updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
-    process.stdout.write(`your name is: ${chunk}`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
